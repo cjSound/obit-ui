4eb689c56396a177d678d7475a1d48c27be6cf67
cjSound <visone.cao@supernovacompanies.biz>
2020-02-27 16:21:50 +0800

    add  api

--- src/ajax/index.js
+++ src/ajax/index.js
@@ -2,14 +2,15 @@
  * @Author: 曹捷
  * @Date: 2019-08-01 14:25:13
  * @LastEditors: 曹捷
- * @LastEditTime: 2020-02-27 14:58:24
+ * @LastEditTime: 2020-02-27 16:12:54
  * @Description: file content
  */
-
+import api from './api/api'
 import common from './api/common-api'
 class obitAjax {
     constructor(apis) {
         this.defultApi = apis
+        this.api = api
         this.methods = {
             ...apis
         }
@@ -21,7 +22,6 @@ class obitAjax {
         }
     }
 }
-console.log('99999999999999999999999999')
 
 export default new obitAjax(common)
 // export default {
