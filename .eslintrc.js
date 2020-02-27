33c5090204c1271de9e93c40b030928ba4abdde4
cjSound <visone.cao@supernovacompanies.biz>
2020-02-27 15:44:09 +0800

    fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,10 @@
+/*
+ * @Author: 曹捷
+ * @Date: 2020-02-26 14:03:03
+ * @LastEditors: 曹捷
+ * @LastEditTime: 2020-02-27 15:42:53
+ * @Description: file content
+ */
 module.exports = {
   root: true,
   env: {
@@ -11,7 +18,7 @@ module.exports = {
     parser: 'babel-eslint'
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // 'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
 }
